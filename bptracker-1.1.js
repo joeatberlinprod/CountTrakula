65340e3216089ba688e56077b99c86ff05b426dc
Joe Faraci <joe@berlinprod.com>
2013-04-22 16:08:46 -0400

    Initial commit.
    bptracker renamed to counttrakula.  Added bptracker for historical
    purposes.

---
+++ bptracker-1.1.js
@@ -0,0 +1,23 @@
+/* 
+TRACK ALL OUTBOUND URLS AND FORM SUBMISSIONS
+<script src="http://berlinprod.com/lab/bptracker/bptracker-1.0.js" type="text/javascript"></script>
+<script src="http://berlinprod.com/lab/bptracker/bptracker-1.0.min.js" type="text/javascript"></script>
+ADD ONCLICK TO THE DOM
+$('a[target="_blank"]').each(function(){ $(this).attr('onclick','_gaq.push([\'_trackEvent\', \'External Link\', $(this).attr(\'href\'), document.title]); setTimeout(\'window.open("'+$(this).attr('href')+'")\', 100); return false;'); });
+TRACK ONCLICK SPECIFICALLY FOR FORM SUBMITS
+$('form a[href*="submit"],form a[onclick*="submit"],form input[type=submit],form input[type=image]').mousedown(function() { _gaq.push(['_trackEvent', 'Form Submit', 'Form Name: '+$(this).parents('form').attr('name')+', Form ID: '+$(this).parents('form').attr('id')+', Form Method: '+$(this).parents('form').attr('method')+', Form Action: '+$(this).parents('form').attr('action'), document.title]); }); 
+
+CHANGE MOUSEDOWN TO USE STANDARD MOUSEDOWN HANDLER
+$('a[target="_blank"], area[target="_blank"]').mousedown(function(){ _gaq.push(['_trackEvent', 'External Link', $(this).attr('href'), document.title]); });
+$('a[target!="_blank"], area[target="!_blank"]').mousedown(function(){ _gaq.push(['_trackEvent', 'Internal Link', $(this).attr('href'), document.title]); });
+
+CHANGE MOUSEDOWN TO USE "ON" HANDLER
+$(document).on('mousedown','a[target="_blank"], area[target="_blank"]',function(){ _gaq.push(['_trackEvent', 'External Link', $(this).attr('href'), document.title]); });
+$(document).on('mousedown','a[target!="_blank"], area[target="!_blank"]',function(){ _gaq.push(['_trackEvent', 'Internal Link', $(this).attr('href'), document.title]); });
+*/
+
+$(function() {
+$(document).on('mousedown','a[target="_blank"], area[target="_blank"]',function(){ _gaq.push(['_trackEvent', 'External Link', $(this).attr('href'), document.title]); });
+$(document).on('mousedown','a[target!="_blank"], area[target="!_blank"]',function(){ _gaq.push(['_trackEvent', 'Internal Link', $(this).attr('href'), document.title]); });
+$("form").submit(function(e) {var self = this;e.preventDefault();_gaq.push(['_trackEvent', 'Form Submit', 'Form Name: '+$(this).attr('name')+', Form ID: '+$(this).attr('id')+', Form Method: '+$(this).attr('method')+', Form Action: '+$(this).attr('action'), document.title]);setTimeout(self.submit(), 250);return false;});
+});
